d013f19db900460107649825efb30bbb50471d27
paulonova <paulonova144@gmail.com>
2015-08-05 14:25:22 +0200

    Commit 01

---
+++ app/src/main/java/com/example/paulorogerio/friendgreeting/MainActivity.java
@@ -0,0 +1,65 @@
+package com.example.paulorogerio.friendgreeting;
+
+import android.app.Activity;
+import android.app.ListActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+
+public class MainActivity extends ListActivity {
+
+    String[] names;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_main);
+
+        names = getResources().getStringArray(R.array.friends);
+        setListAdapter(new ArrayAdapter<String>(this, R.layout.friend_item, names));
+
+
+    }
+
+
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        super.onListItemClick(l, v, position, id);
+
+        Intent in = new Intent(this, SecondActivity.class);
+
+        in.putExtra("message", getString(R.string.show_greetings)+ " " + names[(int) id] + "!" );
+
+        startActivity(in);
+
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
